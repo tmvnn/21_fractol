51e3bb9393870e6ba1ff7cc07785b0f2e01f2104
Тимур Якубов <tmyakubov@bk.ru>
2020-03-31 21:21:39 +0300

    small refactoring

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: timuryakubov <timuryakubov@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/23 22:03:04 by timuryakubo       #+#    #+#             */
-/*   Updated: 2020/03/23 22:09:27 by timuryakubo      ###   ########.fr       */
+/*   Updated: 2020/03/31 21:11:58 by timuryakubo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define FRACTOL_H
 
 # include "libft.h"
+# include <stdio.h>
+# include <errno.h>
 
 # define ABS(number) ((number) > 0 ? (number):-(number))
 # define WIN_HEIGHT 1000
--- srcs/main.c
+++ srcs/main.c
@@ -6,20 +6,30 @@
 /*   By: timuryakubov <timuryakubov@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/23 22:03:05 by timuryakubo       #+#    #+#             */
-/*   Updated: 2020/03/25 23:21:28 by timuryakubo      ###   ########.fr       */
+/*   Updated: 2020/03/31 21:20:52 by timuryakubo      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
+void				*pr_error(char *err_msg)
+{
+	perror(err_msg);
+	exit(1);
+}
+
+int					pr_out(char *out_str)
+{
+	ft_putstr(out_str);
+	return(1);
+}
+
 int					main(int argc, char **argv)
 {
+	errno = 0;
 	argv++;
 	if (argc != 2)
-		ft_putstr("Usage : ./fractol <fract_name>\n");
-	else
-	{
-		ft_putstr("Program was started\n");
-	}
+		return(pr_out("Usage : ./fractol <fract_name>\n"));
+	//render("Program was started\n");
 	return (0);
-} 
+}
